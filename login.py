6afbe07082a9165271771544cd5eac90888b6d57
Zkev77 <kevindmv2007@gmail.com>
2026-05-24 16:06:54 -0400

    cambio en el login

--- login.py
+++ login.py
@@ -1,6 +1,7 @@
+import hashlib
+import tkinter as tk
 import customtkinter
 from interfaz import MenuTaller  
-import tkinter as tk  
 
 customtkinter.set_appearance_mode('dark')
 customtkinter.set_default_color_theme('blue')
@@ -10,12 +11,20 @@ root.geometry('350x300')
 root.title("Acceso al Sistema")
 root.resizable(False, False)
 
-def login():
-    usuario_correcto = "admin"
-    clave_correcta = "1234"
+def validar_login():
+    usuario = entry1.get()
+    clave = entry2.get()
 
-    if entry1.get() == usuario_correcto and entry2.get() == clave_correcta:
-        root.destroy()
+    if not usuario or not clave:
+        print("Por favor, llene todos los campos")
+        return
+
+    CLAVE_FIJA = "03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4"
+
+    clave_hash = hashlib.sha256(clave.encode()).hexdigest()
+
+    if usuario == "admin" and clave_hash == CLAVE_FIJA:
+        root.destroy()  
         
         root_menu = tk.Tk()
         app = MenuTaller(root_menu)
@@ -29,13 +38,14 @@ frame.pack(pady=20, padx=60, fill='both', expand=True)
 label = customtkinter.CTkLabel(master=frame, text='Taller Don Julio', font=('Arial', 18, 'bold'))
 label.pack(pady=12, padx=10)
 
+# Aquí es donde se definen entry1 y entry2 para que la función los pueda leer
 entry1 = customtkinter.CTkEntry(master=frame, placeholder_text='Username')
 entry1.pack(pady=12, padx=10)
 
 entry2 = customtkinter.CTkEntry(master=frame, placeholder_text='Password', show='*')
 entry2.pack(pady=12, padx=10)
 
-button = customtkinter.CTkButton(master=frame, text='Iniciar Sesión', command=login)
+button = customtkinter.CTkButton(master=frame, text='Iniciar Sesión', command=validar_login)
 button.pack(pady=12, padx=10)
 
 root.mainloop()
